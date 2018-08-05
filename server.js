bae196f658e859f6489ae833840ec96ad6674639
dezhizhang <1541609448@qq.com>
2018-08-05 14:22:14 +0800

    创建接口

--- server.js
+++ server.js
@@ -5,6 +5,37 @@ const querystring=require('querystring');
 
 //创建服务
 var server=http.createServer(function(req,res){
+ 
+//解析数据
+  var str='';
+  req.on('data',function(data){
+     str+=data;
+
+  });
+
+  req.on('end',function(){
+      var obj=Liburl.parse(req.url,true);
+      var url=obj.parse;
+      var GET=obj.query;
+
+      var POST=querystring.parse(str);
+
+  })
+  //读取文件
+  if(req.url=='/user'){
+
+  }else{
+      var file_name='./www'+url;
+      fs.readFile(file_name,function(err,data){
+          if(err){
+              res.write('404');
+
+          }else{
+              res.write(data);
+              
+          }
+      })
+  }
 
 });
 
