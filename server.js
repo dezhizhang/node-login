1a7be4736e68a91ff23a5250edb1cbcac706d9b9
dezhizhang <1541609448@qq.com>
2018-08-05 14:04:28 +0800

    创建服务

---
+++ server.js
@@ -0,0 +1,12 @@
+const http=require('http');
+const Liburl=require('url');
+const fs=require('fs');
+const querystring=require('querystring');
+
+//创建服务
+var server=http.createServer(function(req,res){
+
+});
+
+server.listen('8082','localhost');
+
